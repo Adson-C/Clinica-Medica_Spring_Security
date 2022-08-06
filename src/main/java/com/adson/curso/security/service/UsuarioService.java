80979e1f95a420a65c5545e1f473781050e8d9aa
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-08-05 22:57:42 -0300

    Cadastrando novos Usuarios

--- src/main/java/com/adson/curso/security/service/UsuarioService.java
+++ src/main/java/com/adson/curso/security/service/UsuarioService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -72,5 +73,15 @@ public class UsuarioService implements UserDetailsService {
 				: repository.findByEmailOrPerifl(datatables.getSearch(), datatables.getPageable());		
 		
 		return datatables.getResponse(page);
+	}
+	
+	@Transactional(readOnly = false)
+	public void salvarUsuarios(Usuario usuario) {
+		String crypt = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(crypt);
+		
+		
+		repository.save(usuario);
+		
 	}
 }
--- src/main/java/com/adson/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/adson/curso/security/web/controller/UsuarioController.java
@@ -1,13 +1,19 @@
 package com.adson.curso.security.web.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.adson.curso.security.domain.Perfil;
 import com.adson.curso.security.domain.Usuario;
 import com.adson.curso.security.service.UsuarioService;
 
@@ -38,6 +44,28 @@ public class UsuarioController {
 	public ResponseEntity<?> listarUsuarioDatatables(HttpServletRequest request) {
 
 		return ResponseEntity.ok(service.buscarTodos(request));
+	}
+	
+	// salvar cadastro de usuarios por administrador
+	@PostMapping("/cadastro/salvar")
+	public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
+		
+		List<Perfil> perfils = usuario.getPerfis();
+		if (perfils.size() > 2 || 
+			perfils.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) || 
+			perfils.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
+		attr.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico.");
+		
+		attr.addFlashAttribute("usuario", usuario);
+			
+		}
+		else {
+			service.salvarUsuarios(usuario);
+			attr.addFlashAttribute("sucesso", "Operação realizada com Sucesso!");
+		}
+		
+		return "redirect:/u/novo/cadastro/usuario";
+		
 	}
 
 }
