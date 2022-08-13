127d467041c370a51c83a3eb64b3f55b13c33bba
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-08-13 09:06:47 -0300

    Inserindo os dados pessoais do paciente

--- src/main/java/com/adson/curso/security/web/controller/PacienteController.java
+++ src/main/java/com/adson/curso/security/web/controller/PacienteController.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.adson.curso.security.domain.Paciente;
 import com.adson.curso.security.domain.Usuario;
 import com.adson.curso.security.service.PacienteService;
+import com.adson.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("pacientes")
@@ -18,6 +20,9 @@ public class PacienteController {
 	
 	@Autowired
 	private PacienteService service;
+	
+	@Autowired
+	private UsuarioService usuarioService;
 	
 	// abrir paginas de dados pessoais paciente)
 	@GetMapping("/dados")
@@ -28,6 +33,22 @@ public class PacienteController {
 			paciente.setUsuario(new Usuario(user.getUsername()));
 		}
 		model.addAttribute("paciente", paciente);
+		
+		return "paciente/cadastro";
+	}
+	
+	// salvar o form de dados pessoais do paciente com verificação de senha 
+	@PostMapping("/salvar")
+	public String salvar(Paciente paciente, ModelMap model, @AuthenticationPrincipal User user) {
+		Usuario u = usuarioService.buscarPorEmail(user.getUsername());
+		if (UsuarioService.isSenhaCorreta(paciente.getUsuario().getSenha(), u.getSenha())) {
+			paciente.setUsuario(u);
+			service.salvar(paciente);
+			model.addAttribute("sucesso", "Seus dados foram inseridos com sucesso.");
+			
+		} else {
+			model.addAttribute("falha", "Sua senha não confere , tente novamente.");
+		}
 		
 		return "paciente/cadastro";
 	}
