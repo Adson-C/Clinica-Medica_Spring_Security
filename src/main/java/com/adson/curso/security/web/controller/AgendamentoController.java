0eb042fbf9ca4889edf18984e785ef65d2d6cee7
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-08-18 22:22:00 -0300

    Editando agendamento

--- src/main/java/com/adson/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/adson/curso/security/web/controller/AgendamentoController.java
@@ -109,5 +109,19 @@ public class AgendamentoController {
 		return "agendamento/cadastro";
 		
 	}
+	
+	@PostMapping("/editar")
+	public String editarConsulta(Agendamento agendamento, RedirectAttributes attr , @AuthenticationPrincipal User user) {
+		
+		String titulo = agendamento.getEspecialidade().getTitulo();
+		Especialidade especialidade = especialidadeService
+				.buscarPorTitulo(new String[] {titulo})
+				.stream().findFirst().get();
+		agendamento.setEspecialidade(especialidade);
+		
+		service.editar(agendamento, user.getUsername());
+		attr.addFlashAttribute("sucesso", "Sua consulta foi alterada com sucesso");
+		return "redirect:/agendamentos/agendar";
+	}
 
 }
